d7f398567e1afc51d3875ea027e1d8d21062e542
BertZZ <bertie.wooles@hotmail.co.uk>
2018-04-11 14:51:56 +0100

    Raw JSON from express server desplays on REACT frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: JSON.stringify(res) }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/menu');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
   render() {
     return (
-      <div className="App">
-      </div>
+        <p className="App-intro">{this.state.response}</p>
     );
   }
 }
--- node-server/app.js
+++ node-server/app.js
@@ -2,14 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const request = require('request')
 const app = express()
+const port = process.env.PORT || 5000;
 
-app.get('/', function(req, res) {
+app.get('/menu', function(req, res) {
   token = process.env.ACCESS_TOKEN
   request("https://api.flypaythis.com/v2/location/92/menu?accessToken=" + token, function(error, response, body) {
-    res.json(JSON.parse(body))
+    res.send(JSON.parse(body))
   })
 })
 
-app.listen(3000, () => console.log('Lisening on port 3000'))
+app.listen(port, () => console.log('Lisening on port 5000'))
 
 module.exports = app;
