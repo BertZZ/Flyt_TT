ce91385a994839f7e13cc4c0208a8f3b8760d39b
BertZZ <bertie.wooles@hotmail.co.uk>
2018-04-12 20:22:15 +0100

    Creates basic menu component

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,14 @@
 import React, { Component } from 'react';
-
-import logo from './logo.svg';
-
+import Menu from './Menu';
 import './App.css';
 
 class App extends Component {
-  state = {
-    response: ''
-  };
 
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: JSON.stringify(res) }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/menu');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
-  render() {
+render() {
     return (
-        <p className="App-intro">{this.state.response}</p>
+        <div className="App">
+            <Menu />
+        </div>
     );
   }
 }
---
+++ client/src/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Menu () {
+
+// I think i want to call the API here and then pass in everything from this componant. I feel his componant could get kind of bloated however. Try watch out
+
+  return(
+    <h1> "I AM A MENU COMPONENT"</h1>
+  )
+}
+
+export default Menu;
